2193afb93422ea43fd86e411d59c83f3a3c872fb
Stephan Kleinbölting <skleinbo@thp.uni-koeln.de>
2020-02-11 08:56:40 +0100

    New coloring functions
    Better readability

--- src/ColorFunctions.jl
+++ src/ColorFunctions.jl
@@ -8,3 +8,33 @@ function color_gen(state;palette=rand(RGB,maximum(state.lattice.data)))
 		end
 	end |> X->reshape(X, length(v0))
 end
+
+function color_neighbors(state, I::CartesianIndex)
+	nn = neighbors(state.lattice, I)
+	C = similar(state.lattice.data, RGBA{Float32})
+	fill!(C, colorant"gray")
+
+	C[nn] .= colorant"cornflowerblue"
+	return reshape(C, length(C))
+end
+
+function color_column(state, q)
+	C = similar(state.lattice.data, RGBA{Float32})
+	fill!(C, colorant"gray")
+
+	C[:,q] .= colorant"cornflowerblue"
+	return reshape(C, length(C))
+end
+
+function color_row(state, r)
+	C = similar(state.lattice.data, RGBA{Float32})
+	fill!(C, colorant"gray")
+
+	C[r,:] .= colorant"cornflowerblue"
+	return reshape(C, length(C))
+end
+
+function color_rand(state)
+	C = similar(state.lattice.data, RGBA{Float32})
+	reshape(rand(RGB{Float32}, size(state.lattice.data)), length(state.lattice.data))
+end
--- src/VisualGrowthDynamics.jl
+++ src/VisualGrowthDynamics.jl
@@ -1,11 +1,11 @@
 module VisualGrowthDynamics
 
 
-export plot!, plot
+# export plot!, plot
 
 import GrowthDynamics: TumorConfigurations, Lattices
 import .TumorConfigurations: TumorConfiguration
-import .Lattices: HexagonalLattice
+import .Lattices: HexagonalLattice, neighbors
 import AbstractPlotting: Plot, plot!, default_theme, to_value
 
 using GeometryTypes
@@ -26,18 +26,25 @@ function plot!(p::Plot(TumorConfiguration{HexagonalLattice{Int64}}))
 	L = state.lattice.Na
 	@show to_value(p[:color])
 
-    hex_vertices = map(x->[Point2f0(cos(pi/6),sin(pi/6)) Point2f0(-sin(pi/6),cos(pi/6))]*x, [Point2f0(1,0),
+    hex_vertices = map([
+			# Vertices of a hex with flat edge down
+			Point2f0(1,0),
 		    Point2f0(cos(pi/3),-sin(pi/3)),
 		    Point2f0(-cos(pi/3),-sin(pi/3)),
 		    Point2f0(-1,0),
 		    Point2f0(-cos(pi/3),sin(pi/3)),
-		    Point2f0(cos(pi/3),sin(pi/3))]/L/Float32(cos(pi/6)))
+		    Point2f0(cos(pi/3),sin(pi/3))
+			]/L/Float32(cos(pi/6))) do x
+				# Rotation by π/6 --> pointy edge down
+				[Point2f0(cos(pi/6),sin(pi/6)) Point2f0(-sin(pi/6),cos(pi/6))]*x
+			end
 	hex_mesh=GLNormalMesh(hex_vertices)
 	off = (sqrt(5)-sqrt(3))/2
 
-	hex_positions = reshape(Point2f0[Point2f0(2*(xi+ifelse(yi%2==0,0.,1/2))/L-1.,
-		2*(yi+1/2+off/2*ifelse(yi==0,0.,-yi))/L-1.01)
-		 for (xi,yi) in Iterators.product(0:L-1,0:L-1)], L^2)
+	hex_positions = reshape(Point2f0[Point2f0(
+		2*(q+ifelse(r%2==0,0.,1/2))/L-1.0,
+		2*(r+1/2+off/2*ifelse(r==0,0.,-r))/L-1.0)
+		 for (r,q) in Iterators.product(0:L-1,0:L-1)], L^2)
 
 	meshscatter!(p, hex_positions, marker=hex_mesh, markersize=1,
 	 	color=lift(to_value(p[:color]), Node(state)),

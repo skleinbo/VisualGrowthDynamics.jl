4ee6d539356c79ddcee1579d19edc835942662b8
Stephan Kleinbölting <skleinbo@thp.uni-koeln.de>
2020-06-10 18:26:56 +0200

    - genotype keyword when plotting TumorConfiguration{CubicLattice} - cleanup

--- src/VisualGrowthDynamics.jl
+++ src/VisualGrowthDynamics.jl
@@ -5,6 +5,7 @@ import StaticArrays: SVector
 import GrowthDynamics: TumorConfigurations, Lattices
 import .TumorConfigurations: TumorConfiguration
 import .Lattices: HexagonalLattice, CubicLattice, neighbors
+import LightGraphs: neighborhood
 
 using GeometryBasics
 using Colors
@@ -25,7 +26,6 @@ end
 function plot!(p::Plot(TumorConfiguration{HexagonalLattice{Int64}}))
 	state = to_value(p[1])
 	L = state.lattice.Na
-	# @show to_value(p[:color])
 
     hex_vertices = map([
 			# Vertices of a hex with flat edge down
@@ -39,7 +39,7 @@ function plot!(p::Plot(TumorConfiguration{HexagonalLattice{Int64}}))
 				# Rotation by π/6 --> pointy edge down
 				[Point2f0(cos(pi/6),sin(pi/6)) Point2f0(-sin(pi/6),cos(pi/6))]*x
 			end
-	#hex_mesh=GLNormalMesh(hex_vertices)
+
 	hex_mesh=GeometryBasics.mesh(hex_vertices)
 	off = (sqrt(5)-sqrt(3))/2
 
@@ -51,9 +51,6 @@ function plot!(p::Plot(TumorConfiguration{HexagonalLattice{Int64}}))
 	meshscatter!(p, hex_positions, marker=hex_mesh, markersize=1,
 	 	color=lift(to_value(p[:color]), Node(state)),
 		shading=false, raw=false)
-	# meshscatter!(p, hex_positions, markersize=1/64,
-	# 	color=lift(to_value(p[:color]), Node(state)),
-	# 	shading=false)
 
 	p
 end
@@ -62,7 +59,8 @@ end
 function default_theme(scene::SceneLike, ::Type{<: Plot(TumorConfiguration{CubicLattice{Int64}})})
 	Attributes(
 		markersize=1,
-		resolution=(800,800)
+		resolution=(800,800),
+		genotype=1
 	)
 end
 
@@ -71,7 +69,12 @@ function AbstractPlotting.plot!(p::Plot(TumorConfiguration{CubicLattice{Int64}})
 	@show typeof(p)
 	state = to_value(p[1])
 	L = state.lattice.Na
-	# @show to_value(p[:color])
+
+	geno_filter = state.meta.genotypes[
+		neighborhood(state.Phylogeny, to_value(p[:genotype]), typemax(Int64); dir=:in)
+		]
+
+	flt = BitArray( x in geno_filter for x in state.lattice.data)
 
 	if haskey(p.attributes, :plane)
 		P = to_value(p[:plane])
@@ -79,35 +82,24 @@ function AbstractPlotting.plot!(p::Plot(TumorConfiguration{CubicLattice{Int64}})
 		indices = product(1:L, 1:L, 1:L)
 		B = BitArray(Lattices.euclidean_dist(SVector{3}(I), P) <= 1/2 for I in indices)
 		#@show size(B)
-		flt = (state.lattice.data .!= 0) .& B
+		flt = flt .& B
 
 		arrows!(fill(Point3f0(P.p), 3), 10.0*[P.u, P.v, P.w], arrowsize=0.1, arrowlength=10, linewidth=3)
-	else
-		flt = state.lattice.data .!= 0
+	end
+
+	if all(x->x==false, flt)
+		@info "No cells to draw."
 	end
 
 	positions = Point3f0[Point3f0(l,m,n)
 				 for (l,m,n) in Iterators.product(0:L-1,0:L-1,0:L-1)][flt]
     positions = reshape(positions, length(positions))
-	#positions = positions[filter(x->state[x]!=0, 1:length(positions))]
-	#color = lift(X->X[flt] ∘ to_value(p[:color]), p[1])
+
 	color = (to_value(p[:color])(state))[reshape(flt, length(flt))]
 
-
-	# @show typeof(color[])
-	# @show positions[1:10]
     cube = GeometryBasics.HyperRectangle(0,0,0, 1,1,1)
 	meshscatter!(p, positions, marker=cube, markersize=1,
 	 	 color=color, raw=false)
-
-	 if haskey(p.attributes, :plane) && typeof(p.parent)==Scene && typeof(cameracontrols(p.parent))!=EmptyCamera
-		 @info "Adjusting camera..."
-		 cam = cameracontrols(p.parent)
-		 cam.upvector[] = P.u
-		 cam.lookat[] = P.p
-		 cam.eyeposition[] = P.p + L/2*P.w
-		 update_cam!(p.parent, cam)
-	 end
 
 	p
 end

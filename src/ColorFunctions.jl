e921432aa2fcb88f3699dd49cadf9bcaeea4b8ca
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2022-09-28 11:46:55 +0200

    Import Graphs: SimpleDiGraph
    Note: When squashing combine with
    beb8f0d75ebb076e0e621efdafe7ba90697de8a3

--- src/ColorFunctions.jl
+++ src/ColorFunctions.jl
@@ -1,7 +1,7 @@
 module ColorFunctions
 	using Colors
 	import ..Lattices: intersect_lattice_with_plane, dimension, density
-	import Graphs: neighborhood, nv
+	import Graphs: neighborhood, nv, SimpleDiGraph
 	import GrowthDynamics.TumorConfigurations: gindex, TumorConfiguration
 
 	is_leaf(g::SimpleDiGraph, s) = !mapreduce(|, vertices(g)) do in; has_edge(g, in, s) end
